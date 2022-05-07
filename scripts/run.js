765aca08485892407dd1007b942a3ae75a1c819d
Jake Mills <jakelmills@protonmail.com>
2022-05-07 23:24:27 +0100

    added payment functionalityand setup tld

--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,19 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners()
   const domainContractFactory = await hre.ethers.getContractFactory('Domains')
-  const domainContract = await domainContractFactory.deploy()
+  const domainContract = await domainContractFactory.deploy('everything')
   await domainContract.deployed()
   console.log('Contact deployed to:', domainContract.address)
   console.log('Contract deployed by:', owner.address);
 
-  const txn = await domainContract.register('doom')
+  let txn = await domainContract.register('mortal', {value: hre.ethers.utils.parseEther('0.1')})
   await txn.wait()
 
-  const domainOwner = await domainContract.getAddress('doom')
-  console.log('Owner of domain:', domainOwner)
+  const address = await domainContract.getAddress("mortal")
+  console.log('owner of domain seer', address)
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address)
+  console.log('contact balance:', hre.ethers.utils.formatEther(balance))
 }
 
 const runMain = async () => {
